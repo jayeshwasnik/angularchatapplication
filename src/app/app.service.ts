6ddd3f891d702511fcb75b8a3b8a21fa753b6688
jayeshwasnik <jayesh.wasnik.ece12@itbhu.ac.in>
2019-11-18 22:35:35 +0530

    1 file modified

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -6,11 +6,37 @@ import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/toPromise';
 
-//to use 
+//to use http params
+import{HttpHeaders,HttpParams} from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http'; 
 
 @Injectable()
 export class AppService {
 
-  constructor() { }
+  public baseUrl="https://chatapi.edwisor.com/api/v1/";
+  
+public apiKey='NzE3YTZlODRmYzI3NzczMDc3MjM2YjJmOTFmZTkzMTM5YTk0ZjQ0NmI5YjUyYTQ3ODhhYzQ3OThhZjI5ODEzMzdiYzVmMDRhMDcyMWQzZjQ5MWIzNmEwOTRmNjQ5ODcyZmUzZmYzOGI1NzY2ZGE1NmRjMTVkYWRlM2NmZWYxZmRkNw==';
+
+  constructor(public http:HttpClient) { }
+
+  public userSignup(data){
+    const params=new HttpParams()
+    .set("firstName",data.firstName)
+      .set("password",data.password)
+        .set("lastName",data.lastName)
+          .set("email",data.email)
+            .set("mobileNumber	",data.mobileNumber	)
+              .set("apiKey",this.apiKey);
+   this.http.post(`${this.baseUrl}/users/signup`,params);
+
+  }
+
+  public userLogin(data){
+    const params=new HttpParams()
+      .set("password",data.password)
+          .set("email",data.email);
+   this.http.post(`${this.baseUrl}/users/login`,params);
+
+  }
 
 }
